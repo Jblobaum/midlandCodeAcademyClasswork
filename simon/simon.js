c3eac5e45a96f4de3c2e57d80f97cbbc66ccdd78
Justin Blobaum <justinblobaum@justins-air.mlc.edu>
2020-01-31 16:49:10 -0600

    got simon's picks

--- simon/simon.js
+++ simon/simon.js
@@ -5,87 +5,90 @@ let yellow = document.getElementById("yellow");
 let blue = document.getElementById("blue");
 
 
-// Simon Levels
-for (let i = 1; i <= 5; i++) {
-    let x = "*";
-    for (let j = 1; j < i; j++) {
-        x = x + "*";
-    }
-    console.log(x)
-}
-
-
 for (i = 0; i < simonButton.length; i++) {
     simonButton[i].addEventListener("mousedown", function () {
-        if (this === red) {
-            document.getElementById("red").classList.add("active");
-            console.log("red");
-            
-        } else if (this === green) {
-            console.log("green");
-            document.getElementById("green").classList.add("active");
-           
-        } else if (this === yellow) {
-            document.getElementById("yellow").classList.add("active");
-            
-        } else if (this === blue) {
-            console.log("blue");
-            document.getElementById("blue").classList.add("active");
-            
-            
-        }
+        this.classList.add("active");
     })
 }
 for (i = 0; i < simonButton.length; i++) {
     simonButton[i].addEventListener("mouseup", function () {
-        if (this === red) {
-            document.getElementById("red").classList.remove("active");
-        
-            
-        } else if (this === green) {
-            console.log("green");
-            document.getElementById("green").classList.remove("active");
-            
-
-        } else if (this === yellow) {
-            document.getElementById("yellow").classList.remove("active");
-            
-
-        } else if (this === blue) {
-            console.log("blue");
-            document.getElementById("blue").classList.remove("active");
-
-        }
-
+        this.classList.remove("active");
     })
 
 }
-
+let counter = 0;
+let round = 0;
+let simonArray = [];
+let arrayMax = 100;
+let limit = arrayMax + 1;
+for (var i = 0; i < arrayMax; i++) {
+    simonArray.push(Math.floor(Math.random() * 4));
+}
+console.log(simonArray)
 
 
 function computerChoice() {
-    let comp = Math.floor(Math.random() * 4)
-    console.log(comp);
-    if (comp === 0) {
-        document.getElementById("red").classList.add("active");
-        setTimeout(function(){
-            document.getElementById("red").classList.remove("active");
-        }, 500)
-    } else if (comp === 1) {
-        document.getElementById("green").classList.add("active");
-        setTimeout(function(){
-            document.getElementById("green").classList.remove("active");
-        }, 500)
-    } else if (comp === 2) {
-        document.getElementById("yellow").classList.add("active");
-        setTimeout(function(){
-            document.getElementById("yellow").classList.remove("active");
-        }, 500)
-    } else if (comp === 3) {
-        document.getElementById("blue").classList.add("active");
-        setTimeout(function(){
-            document.getElementById("blue").classList.remove("active");
-        }, 500)
+    if (counter > round) {
+        counter = 0;
+    } else {
+        let comp = simonArray[counter];
+        console.log(comp);
+        if (comp === 0) {
+            document.getElementById("red").classList.add("active");
+            setTimeout(function () {
+                document.getElementById("red").classList.remove("active");
+                counter++;
+                computerChoice()
+            }, 500)
+        } else if (comp === 1) {
+            document.getElementById("green").classList.add("active");
+            setTimeout(function () {
+                document.getElementById("green").classList.remove("active");
+                counter++;
+                computerChoice()
+            }, 500)
+        } else if (comp === 2) {
+            document.getElementById("yellow").classList.add("active");
+            setTimeout(function () {
+                document.getElementById("yellow").classList.remove("active");
+                counter++;
+                computerChoice()
+            }, 500)
+        } else if (comp === 3) {
+            document.getElementById("blue").classList.add("active");
+            setTimeout(function () {
+                document.getElementById("blue").classList.remove("active");
+                counter++;
+                computerChoice()
+            }, 500)
+        }
     }
 }
 computerChoice()
+// computerChoice()
+// computerChoice()
+// computerChoice()
+// computerChoice()
+// computerChoice()
+// computerChoice()
+// computerChoice()
+// computerChoice()
+// computerChoice()
+// computerChoice()
+// computerChoice()
+// computerChoice()
+
+
+
+// simon=[0, 1, 2, 3]
+
+// for (let i = 1; i <= 5; i++) {
+//     let x = simonButton;
+//     for (let j = 1; j < i; j++) {
+//         x = x + simonButton;
+//     }
+// console.log(x)
+// Chris Trying Stuff
+
+
+// let userArray = [];
